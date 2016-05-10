c210fe158a864ad4aa32bf3c4a01216dc63f9011
alexseth1124 <alexseth1124@gmail.com>
2016-05-10 18:06:16 -0400

    Done

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,20 @@
-# Enter your procedural solution here!
+def largest_prime_factor(value)
+  (2..value).each do |x|
+    if value % x == 0 && prime?(value / x)
+      return (value / x)
+      break
+    else
+      x += 1
+    end
+  end
+end
+
+def prime?(number)
+  !(2..number/2.to_i).any? {|x| number % x == 0}
+end
+
+# Operate on a range from 2 to the value.
+# If x (a number in the range) is divisible by the value, it's a factor.
+# If (value / x) is prime, that has to be the largest prime number 
+# if you're going from smallest to largest with x.
+# If it's not, keep going.
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,24 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def number
+    (2..value).each do |x|
+      if value % x == 0 && prime?(value / x)
+        return (value / x)
+        break
+      else
+        x += 1
+      end
+    end
+  end
+
+
+  def prime?(number)
+    !(2..number/2.to_i).any? {|x| number % x == 0}
+  end
+end
